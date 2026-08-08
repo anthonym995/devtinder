6dfa9b74f6f489bc514f1cb61db7574f4a828742
Anthony M <manthonysuresh@gmail.com>
2026-08-08 11:42:34 +0530

    minor fix

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token) {
-      res.status(401).send("Please login");
+      return res.status(401).send("Please login");
     }
 
     const { _id } = await jwt.verify(token, "Anthony@123");
